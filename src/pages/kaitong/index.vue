32dabea60c826a3c73f8e267f3ec50d89ea90f58
wangy16 <wangy16@knownsec.com>
2020-05-20 18:30:34 +0800

    feat: 添加所有路由

---
+++ src/pages/kaitong/index.vue
@@ -0,0 +1,231 @@
+<template>
+  <div class="kt">
+    <div class="kt_top">
+      <img src="/static/images/开通找店会员icon/icon-zdhy-bg@3x.png" alt="">
+      <div class="kt_top_info">
+        <img src="/static/images/开通找店会员icon/tx.jpg" alt="">
+        <div>
+          <p>用户ID：13311118888</p>
+          <span>您还不是会员哦~</span>
+        </div>
+      </div>
+    </div>
+    <div class="kt_s1">
+      <h1>会员特权</h1>
+      <div class="kt_s1_item">
+        <img src="/static/images/开通找店会员icon/icon-zdhy-mfck@3x.png" alt="">
+        <p>免费查看</p>
+      </div>
+      <div class="kt_s1_item">
+        <img src="/static/images/开通找店会员icon/icon-zdhy-mffb@3x.png" alt="">
+        <p>免费发布</p>
+      </div>
+      <div class="kt_s1_item">
+        <img src="/static/images/开通找店会员icon/icon-zdhy-msyj@3x.png" alt="">
+        <p>免收佣金</p>
+      </div>
+      <div class="kt_s1_item">
+        <img src="/static/images/开通找店会员icon/icon-zdhy-ztfd@3x.png" alt="">
+        <p>直通房东</p>
+      </div>
+    </div>
+    <div class="kt_s2" style="background-image: url('/static/images/开通找店会员icon/icon-zdhy-ljzx-bg@3x.png')">
+      <div class="kt_s2_left">
+        <img src="/static/images/开通找店会员icon/icon-zdhy-vip@3x.png" alt="">
+        <p>更多升级服务请点击咨询</p>
+      </div>
+      <div class="kt_s2_right">
+        <img src="/static/images/开通找店会员icon/icon-zdhy-ljzx-rectangle@3x.png" alt="">
+        <p>立即咨询</p>
+      </div>
+    </div>
+    <div class="kt_s3">
+      <h1>开通会员</h1>
+      <div class="kt_s3_content">
+        <div class="kt_s3_1">
+          <img src="/static/images/开通找店会员icon/icon-zdhy-kthy-bg1@3x.png" alt="">
+          <div>
+            <p>10天</p>
+            <p>￥<span>9.8</span></p>
+            <p style="opacity:0.5;">原价:￥<em style="text-decoration:underline">39</em></p>
+            <p style="opacity:0.5;">(仅享受一次)</p>
+          </div>
+        </div>
+        <div class="kt_s3_2">
+          <img src="/static/images/开通找店会员icon/icon-zdhy-kthy-bg2@3x.png" alt="">
+          <div>
+            <p>30天</p>
+            <p>￥<span>68</span></p>
+            <p opacity:0.5;>原价:￥<em style="text-decoration:underline">118</em></p>
+            <p opacity:0.5;>(仅享受一次)</p>
+          </div>
+        </div>
+        <div class="kt_s3_3">
+          <img src="/static/images/开通找店会员icon/icon-zdhy-kthy-bg3@3x.png" alt="">
+          <div>
+            <p>30天</p>
+            <p>￥<span>68</span></p>
+            <p opacity:0.5;>原价:￥<em style="text-decoration:underline">118</em></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="kt_s4">
+      <div class="kt_s4_1">
+        <img src="/static/images/开通找店会员icon/icon-zdhy-choose@3x.png" alt="">
+        <p>开通即视为同意<span style="color: #EC514E">《会员服务协议》</span></p>
+      </div>
+      <div style="background-image: url('/static/images/开通找店会员icon/icon-zdhy-btn@3x.png')" class="kt_s4_2">
+        <p>立即开通</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="scss" scoped>
+  .kt {
+    width: 670px;
+    margin: 0 auto;
+    .kt_top {
+      img {
+        width: 670px;
+        height: 386px;
+      }
+      .kt_top_info {
+        display: flex;
+        img {
+          width: 104px;
+          height: 104px;
+        }
+        p {
+          color: #fff;
+          font-size: 24px;
+        }
+        span {
+          color: #fff;
+          font-size: 18px;
+        }
+      }
+    }
+    .kt_s1 {
+      h1 {
+        color: #1A1A1A;
+        font-size: 30px;
+        font-weight: bold;
+      }
+      .kt_s1_item {
+        img {
+          width: 97px;
+          height: 97px;
+        }
+        p {
+          color: #333;
+          font-size: 22px;
+        }
+      }
+    }
+    .kt_s2 {
+      position: relative;
+      height: 82px;
+      display: flex;
+      background-size: 670px 82px;
+      background-repeat: no-repeat;
+      .kt_s2_left {
+        display: flex;
+        img {
+          width: 24px;
+          height: 24px;
+          margin-right: 28px;
+        }
+        p {
+          font-size: 28px;
+          color: #EC514E;
+          font-weight: 500;
+        }
+      }
+      .kt_s2_right {
+        position: relative;
+        img {
+          width: 128px;
+          height: 44px;
+        }
+        p {
+          color: #EC514E;
+          font-size: 22px;
+          position: absolute;
+          top: 10px;
+          left: 20px;
+        }
+      }
+    }
+    .kt_s3 {
+      h1 {
+        font-size: 30px;
+        font-weight: bold;
+        color: #1A1A1A;
+      }
+      .kt_s3_content {
+        display: flex;
+        justify-content: space-between;
+        height: 254px;
+        align-items: flex-end;
+        p {
+          color: #333;
+          font-size: 26px;
+        }
+        .kt_s3_1 {
+          img {
+            width: 200px;
+            height: 236px;
+          }
+          p {
+            color: #FF4676;
+            font-size: 26px;
+            span {
+              font-size: 50px;
+            }
+            em {
+              text-decoration: underline;
+            }
+          }
+        }
+        .kt_s3_2 {
+          img {
+            width: 200px;
+            height: 253px;
+          }
+        }
+        .kt_s3_3 {
+          img {
+            width: 200px;
+            height: 236px;
+          }
+        }
+      }
+    }
+    .kt_s4 {
+      .kt_s4_1 {
+        display: flex;
+        p {
+          color: #999;
+          font-size: 22px;
+        }
+      }
+      .kt_s4_2 {
+        width: 670px;
+        height: 85px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        font-size: 30px;
+        margin-top: 14px;
+      }
+    }
+  }
+</style>
---
+++ src/pages/shangpuxiangqin/index.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="spxq">
+    <div class="spxq_top">
+      <img src="/static/images/商铺详情icon/bg1.jpg" alt="">
+    </div>
+    <div class="spxq_s1">
+      <h1>望海路软件园二期公寓低价转让</h1>
+      <div class="spxq_s1_1">
+        <p><img src="/static/images/商铺详情icon/icon-shop-hot@3x.png" alt="">热门</p>
+        <p>便利超市</p>
+        <p>十字或拐角</p>
+        <p>其他</p>
+      </div>
+      <div class="spxq_s1_2">
+        <div>
+          <p>面积（㎡）</p>
+          <span>60</span>
+        </div>
+        <div>
+          <p>租金（元/月）</p>
+          <span>60</span>
+        </div>
+        <div>
+          <p>转让费（万元/月）/月）</p>
+          <span>60</span>
+        </div>
+      </div>
+    </div>
+    <div class="spxq_s2">
+      <div>
+        <p>区域：<span>鼓楼区-西洪路</span></p>
+        <p>商圈：<span>金牛山</span></p>
+      </div>
+      <div>
+        <p>小区：<span>万隆花园一期</span></p>
+        <p>面宽：<span>4米</span></p>
+      </div>
+      <div>
+        <p>进深：<span>4米</span></p>
+        <p>层高：<span>4米</span></p>
+      </div>
+      <div>
+        <p>地址：<span>鼓楼区西洪路457-2美华隆佳便利</span></p>
+        <img src="/static/images/商铺详情icon/ckdt.jpg" alt="">
+      </div>
+      <div>
+        <p>地址：<span>望海路软件园二期公寓靠近地铁商超，周边交通便利，生活环境优越...</span></p>
+      </div>
+    </div>
+    <div class="spxq_s3">
+      <img src="/static/images/商铺详情icon/icon-shop-ad@3x.png" alt="">
+    </div>
+    <div class="spxq_s4">
+      <h1>
+        猜你喜欢
+      </h1>
+      <secitem v-for="item in 10" :key="item"></secitem>
+    </div>
+    <div class="spxq_bottom">
+      <div class="spxq_b1">
+        <img src="/static/images/商铺详情icon/icon-shop-wsc@3x.png" alt="" @click="showRed = true" v-if="!showRed">
+        <img src="/static/images/商铺详情icon/icon-shop-ysc@3x.png" alt="" @click="showRed = false" v-else>
+        <p>收藏</p>
+      </div>
+      <div class="spxq_b1">
+        <p>已租提醒</p>
+      </div>
+      <div class="spxq_b1">
+        <p>直通房东</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import secitem from '@/components/secitem'
+  export default {
+    components: {
+      secitem,
+    },
+    data() {
+      return {
+        showRed: false,
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .spxq {
+    width: 710px;
+    margin: 0 auto;
+    .spxq_top {
+      margin-top: 20px;
+      img {
+        width: 710px;
+        height: 316px;
+      }
+    }
+    .spxq_s1 {
+      h1 {
+        font-size: 36px;
+        font-weight: bold;
+        color: rgba(53, 53, 53, 1);
+        line-height: 70px;
+      }
+      .spxq_s1_1 {
+        p {
+          font-size: 22px;
+          font-weight: 400;
+          color: rgba(236, 81, 78, 1);
+          line-height: 36px;
+          padding: 5px 14px;
+          border-radius: 6px;
+        }
+        img {
+          width: 18px;
+          height: 20px;
+        }
+      }
+      .spxq_s1_2 {
+        display: flex;
+        width: 710px;
+        justify-content: space-between;
+        border-bottom: 1px solid #E3E3E3;
+        p {
+          font-size: 24px;
+          font-weight: 400;
+          color: rgba(118, 118, 118, 1);
+        }
+        span {
+          font-size: 30px;
+          font-weight: bold;
+          color: rgba(236, 81, 78, 1);
+        }
+      }
+    }
+    .spxq_s2 {
+      div {
+        color: #767676;
+        font-size: 24px;
+        display: flex;
+        span {
+          color: #353535;
+        }
+      }
+    }
+    .spxq_s3 {
+      img {
+        width: 710px;
+        height: 264px;
+      }
+    }
+    .spxq_s4 {
+      h1 {
+        font-size: 34px;
+        color: #1A1A1A;
+      }
+    }
+    .spxq_bottom {
+      width: 750px;
+      position: fixed;
+      height: 114px;
+      display: flex;
+      border-top: 1px solid #ddd;
+      .spxq_b1 {
+        width: 170px;
+        img {
+          width: 40px;
+          height: 38px;
+        }
+        p {
+          font-size: 24px;
+          font-weight: 500;
+          color: rgba(114, 118, 119, 1);
+        }
+      }
+      .spxq_b2 {
+        width: 290px;
+        height: 114px;
+        background: #FEAF01;
+        p {
+          font-size: 36px;
+          font-weight: 500;
+          color: rgba(255, 255, 255, 1);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      }
+      .spxq_b3 {
+        width: 290px;
+        height: 114px;
+        background: #EC514E;
+        p {
+          font-size: 36px;
+          font-weight: 500;
+          color: rgba(255, 255, 255, 1);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      }
+    }
+  }
+</style>
